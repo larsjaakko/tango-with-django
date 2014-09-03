e2824cf509ddae7db455c448b2304ad11dcc26f4
Lars Jaakko Havro <larsjh@gmail.com>
2014-09-03 17:48:07 +0200

    Added templates folder and a proof-of-concept context rendering

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,5 @@ from rango import views
 urlpatterns = patterns('',
 
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.about, name='about')
+    url(r'^about', views.about, name='about')
     )
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -81,3 +83,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_DIR,
+)
