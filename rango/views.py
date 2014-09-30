ffd48b86c37b20f15eb9e2941b45ff5ecebca639
larsjaakko <larsjh@gmail.com>
2014-09-30 16:09:53 +0200

    Tried to fix author name on windows workstation

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+
 def index(request):
     # Request the context of the request.
     # The context contains information such as the client's machine details, for example.
