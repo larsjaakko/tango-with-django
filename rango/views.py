4ef104f0648c6a49ad6cb246eda86a02ce0e3ecc
Lars Jaakko Havro <larsjh@gmail.com>
2014-11-03 21:32:07 +0100

    Added forms.py (unfinished)

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,7 @@ from rango.urlencode import encode, decode
 from rango.models import Category
 from rango.models import Page
 
+
 def index(request):
     # Request the context of the request.
     # The context contains information such as the client's machine details, for example.
@@ -16,7 +17,7 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    #context_dict = {'categories': category_list}
+    # context_dict = {'categories': category_list}
 
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
@@ -25,7 +26,7 @@ def index(request):
     # We loop through each category returned, and create a URL attribute.
     # This attribute stores an encoded URL (e.g. spaces replaced with underscores).
     for category in category_list:
-        category.url = encode(category.name) #category.name.replace(' ', '_')
+        category.url = encode(category.name)  # category.name.replace(' ', '_')
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -34,7 +35,7 @@ def index(request):
 
 
 def about(request):
-     # Request the context of the request.
+    # Request the context of the request.
     # The context contains information such as the client's machine details, for example.
     context = RequestContext(request)
 
@@ -47,14 +48,15 @@ def about(request):
     # Note that the first parameter is the template we wish to use.
     return render_to_response('rango/about.html', context_dict, context)
 
+
 def category(request, category_name_url):
     # Request our context from the request passed to us
-    context =   RequestContext(request)
+    context = RequestContext(request)
 
     # Change underscores in the category name to spaces.
     # URLs don't handle spaces well, so we encode them as underscores
     # We can then simply replace the underscores with spaces again to get the name
-    category_name = decode(category_name_url) # .replace('_',' ')
+    category_name = decode(category_name_url)  # .replace('_',' ')
 
     # Create a context dictionary which we can pass to the template rendering engine
     # We start by containing the name of the category passed by the user.
@@ -82,5 +84,3 @@ def category(request, category_name_url):
 
     # Go render the response and return it to the client
     return render_to_response('rango/category.html', context_dict, context)
-
-
