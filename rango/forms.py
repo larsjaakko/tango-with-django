48174d0df1a73c1c123ff44343d936abf30b9432
Lars Jaakko Havro <larsjh@gmail.com>
2014-11-04 22:55:50 +0100

    Added the add_category view and template, upgraded to Django 1.7

--- rango/forms.py
+++ rango/forms.py
@@ -5,7 +5,7 @@ from rango.models import Page, Category
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    likes = forms.IntegerField(wiget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     # slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     # An inline class to provide additional information on the form.
