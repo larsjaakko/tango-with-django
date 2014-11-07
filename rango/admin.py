dd7ee0b1365baf98247f61aeab44491fd563d24c
larsjaakko <larsjh@gmail.com>
2014-11-07 14:16:48 +0100

    Added userprofile model and user registration view

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 admin.site.register(Category)
 # admin.site.register(Page)
@@ -9,5 +9,6 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
     pass
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
 # Register your models here.
