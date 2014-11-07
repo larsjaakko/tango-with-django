949f0a67c5711e960d876553c45ce3bee0267649
larsjaakko <larsjh@gmail.com>
2014-11-07 15:55:42 +0100

    Added login/logout functionality.

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 from rango.urlencode import encode, decode
 
 from rango.models import Category, Page
@@ -150,6 +153,7 @@ def add_page(request, category_name_url):
     return render(request,'rango/add_page.html', {'category_name_url': category_name_url,
              'category_name': category_name, 'form': form})
 
+
 def register(request):
 
     # A boolean value for telling the template whether the registration was successful
@@ -208,6 +212,52 @@ def register(request):
         {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
 
+def user_login(request):
+
+    # If the request is a HTTP POST, try to pull out the relevant information
+    if request.method == 'POST':
+        # Gather the username and password provided by the user
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Use Django's machinery to attempt to see if the username and password
+        # combination is valid - a User object is returned if it is
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled
+            if user.is_active:
+                # If the account is valid and active, we can log the user in
+                # We'll send the user back to the homepage
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                # Bad login details were provided. So we can't log the user in.
+                print "Invalid login details: {0}, {1}".format(username, password)
+                return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form
+    # this scenario would most likely be a HTTP GET
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'rango/login.html', {})
 
 
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them oyt
+    logout(request)
+
+    # Take the user back to the homepage
+    return HttpResponseRedirect('/rango/')
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -101,3 +101,5 @@ TEMPLATE_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL = '/rango/login/'
