82c10c516b1826de28ace1795e5951a1acb5d229
larsjaakko <larsjh@gmail.com>
2015-01-09 12:59:07 +0100

    Added django-registration-redux, refactored code and provided custom MyRegistrationView

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango'
+    'rango',
+    'registration'  # add in the registration package
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,4 +103,13 @@ TEMPLATE_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = '/rango/login/'
+REGISTRATION_OPEN = True                # If True, users can register
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7
+# If True, the user will be automatically logged in.
+REGISTRATION_AUTO_LOGIN = True
+# The page you want users to arrive at after they successful log in
+LOGIN_REDIRECT_URL = '/rango/'
+# The page users are directed to if they are not logged in,
+# and are trying to access pages requiring authentication
+LOGIN_URL = '/accounts/login/'
